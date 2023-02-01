8f70110a37925e89abd0e7695b50f049949968a0
Elian Silva <you@example.com>
2023-02-01 16:24:55 -0300

    ajustado script

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const hello = document.querySelector(".hello-friend");
 const welcome = document.querySelector(".welcome-back");
 const cover = document.querySelector(".cover");
-window.onload = () => {
-  hello.classList.add("active");
-  welcome.classList.add("hide");
-};
-
 const btn = document.querySelectorAll(".login-btn");
 
 const changeScreen = () => {
   cover.classList.toggle("left");
   hello.classList.toggle("active");
   welcome.classList.toggle("active");
+};
+
+window.onload = () => {
+  hello.classList.add("active");
+  welcome.classList.add("hide");
 };
 
 btn.forEach((btn) => btn.addEventListener("click", changeScreen));
